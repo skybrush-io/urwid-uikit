d1444add7be935237d2cb47a517894ab2cadf10a
Tamas Nepusz <ntamas@gmail.com>
2019-04-13 22:13:33 +0200

    added SelectableText

---
+++ urwid_uikit/text.py
@@ -0,0 +1,17 @@
+"""Text-related urwid widgets."""
+
+from urwid import Text
+
+__all__ = ("SelectableText", )
+
+
+class SelectableText(Text):
+    """Subclass of Text_ that can be selectable. Useful as a child widget in
+    list boxes.
+    """
+
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        return key
