a3841c3053e818ed28c0fcab2903f311280da815
Tamas Nepusz <ntamas@gmail.com>
2020-01-20 12:55:48 +0100

    removed unneeded patch in CustomTextProgressBar.render() now that we are on urwid 2.1.0

--- urwid_uikit/progress_bar.py
+++ urwid_uikit/progress_bar.py
@@ -23,42 +23,6 @@ class CustomTextProgressBar(ProgressBar):
         self._has_error = False
         self._has_warning = False
 
-    def render(self, size, focus=False):
-        """
-        Render the progress bar.
-        """
-        # This is copied straight from urwid after incorporating a fix
-        # for Github issue #317. This can be removed once a fix is merged.
-        (maxcol,) = size
-        txt = Text(self.get_text(), self.text_align, CLIP)
-        c = txt.render((maxcol,))
-
-        cf = float(self.current) * maxcol / self.done
-        ccol_dirty = int(cf)
-        ccol = len(c._text[0][:ccol_dirty].decode("utf-8", "ignore").encode("utf-8"))
-        cs = 0
-        if self.satt is not None:
-            cs = int((cf - ccol) * 8)
-        if ccol < 0 or (ccol == 0 and cs == 0):
-            c._attr = [[(self.normal, maxcol)]]
-        elif ccol >= maxcol:
-            c._attr = [[(self.complete, maxcol)]]
-        elif cs and ord2(c._text[0][ccol]) == 32:
-            t = c._text[0]
-            cenc = self.eighths[cs].encode("utf-8")
-            c._text[0] = t[:ccol] + cenc + t[ccol + 1 :]
-            a = []
-            if ccol > 0:
-                a.append((self.complete, ccol))
-            a.append((self.satt, len(cenc)))
-            if maxcol - ccol - 1 > 0:
-                a.append((self.normal, maxcol - ccol - 1))
-            c._attr = [a]
-            c._cs = [[(None, len(c._text[0]))]]
-        else:
-            c._attr = [[(self.complete, ccol), (self.normal, maxcol - ccol)]]
-        return c
-
     def get_text(self):
         """Returns the text to be shown on the progress bar."""
         percent = min(100, max(0, int(self.current * 100 / self.done)))
